139f3598eca3becef744d004f88d29d629ed7a66
raaauf6933 <no.email@example.com>
2023-11-15 13:46:04 +0800

    refactor: update as of nov 15

--- src/app/admin/components/ActionBar/index.js
+++ src/app/admin/components/ActionBar/index.js
@@ -5,7 +5,7 @@ const ActionBar = (props) => {
 
   return (
     <div className="fixed bottom-0 z-10">
-      <div className="bg-white shadow-2xl drop-shadow-2xl shadow-black p-4 w-screen sm:max-w-sm md:sm:max-w-sm lg:max-w-2xl xl:max-w-7xl ">
+      <div className="bg-white shadow-2xl drop-shadow-2xl shadow-black p-4 w-screen sm:max-w-sm md:sm:max-w-sm lg:max-w-2xl xl:max-w-3xl ">
         <div className="flex justify-between">
           <Button
             className="bg-white text-black"
